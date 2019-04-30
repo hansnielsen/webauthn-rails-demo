d02539376d46cf5dde643dd5cf032c4dc856cec5
Hans Nielsen <hans@stackallocated.com>
2019-04-29 19:31:49 -0700

    Add controller for handling U2F

---
+++ app/controllers/two_factor_controller.rb
@@ -0,0 +1,19 @@
+class TwoFactorController < ApplicationController
+  def index
+  end
+
+  def destroy
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def sign
+  end
+
+  def validate
+  end
+end
