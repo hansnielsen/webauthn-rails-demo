e8fa5818c58d2f838a877e41ef40c63a8250075a
Hans Nielsen <hans@stackallocated.com>
2019-04-29 20:41:59 -0700

    Add U2F registration index and delete

--- app/controllers/two_factor_controller.rb
+++ app/controllers/two_factor_controller.rb
@@ -1,8 +1,13 @@
 class TwoFactorController < ApplicationController
   def index
+    @regs = Registration.all
   end
 
   def destroy
+    r = Registration.find_by_id(params[:id])
+    flash[:warning] = "Deleted key #{params[:id]} (#{r.name})"
+    r.destroy
+    redirect_to action: "index"
   end
 
   def new
