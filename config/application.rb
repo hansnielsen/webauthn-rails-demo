0eca75e14d70d0fe6a5f2d6051f4ddf9cfa6cba3
Hans Nielsen <hans@stackallocated.com>
2019-04-29 21:35:22 -0700

    Add registration page and 2FA flow

--- config/application.rb
+++ config/application.rb
@@ -26,5 +26,7 @@ module DemoApp
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    config.app_id = "https://localhost:3000"
   end
 end
