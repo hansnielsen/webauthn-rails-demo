4cded43d9206a464a89325e44119ff74b293f442
Hans Nielsen <hans@stackallocated.com>
2019-05-18 15:15:06 -0700

    Remove U2F shim library

--- app/assets/javascripts/two_factor.js
+++ app/assets/javascripts/two_factor.js
@@ -1,5 +1,4 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 
-//= require u2f-api.js
 //= require base64.js
